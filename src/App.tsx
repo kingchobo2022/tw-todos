cf6651d84a03f3a0445f6d655bd9f75ae9ea16bf
kingchobo <kingchobo2022@gmail.com>
2025-11-22 14:14:27 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main className="py-8 h-screen">
+      <h1 className="font-bold">나의 일정</h1>
+    </main>
+  )
+}
+
+export default App
---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import type { Todo } from "../types/todos";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "이 비디오에 좋아요 해줘요",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "왕초보 홈페이지 만들기 채널 구독하기",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "댓글 달기",
+        completed: false,
+    },
+];
