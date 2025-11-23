bfa0accebb67c67867b14e02c218d163819aa851
kingchobo <kingchobo2022@gmail.com>
2025-11-23 13:48:09 +0900

    3rd commit Add Todo

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,14 +2,19 @@ import type { Todo } from "../types/todos"
 
 interface TodoItemProps {
     todo: Todo;
+    onCompletedChange: (id: number, completed: boolean) => void;
 }
 
 
-export default function TodoItem({todo}: TodoItemProps) {
+export default function TodoItem({todo, onCompletedChange}: TodoItemProps) {
     return (
         <div>
             <label className="flex items-center gap-2 border border-gray-400 rounded-m p-2 bg-white hover:bg-slate-500">
-                <input type="checkbox" className="scale-125" />
+                <input 
+                type="checkbox" 
+                checked={todo.completed} 
+                onChange={ (e) => onCompletedChange(todo.id, e.target.checked)}
+                className="scale-125" />
                 <span className={todo.completed ? "line-through text-gray-400" : ""}>{todo.title}</span>
             </label>
         </div>
